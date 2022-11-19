c3c6d6ea61f568ec622bf996ec26295297d81851
Joshua Bourgeot <dragonsploder@gmail.com>
2022-11-19 15:03:06 -0500

    Get search

--- src/main.go
+++ src/main.go
@@ -1,11 +1,28 @@
 package main
 
 import (
-    "fmt"
-    "os"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
 )
 
 func main() {
     args := os.Args[1:]
     fmt.Println(args)
+
+    url := "https://www.github.com/search?q=" + args[0]
+    resp, err := http.Get(url)
+    if err != nil {
+        log.Fatalln(err)
+    }
+    defer resp.Body.Close()
+
+    body, err := ioutil.ReadAll(resp.Body)
+    if err != nil {
+        log.Fatalln(err)
+    }
+
+    fmt.Println(string(body))
 }
