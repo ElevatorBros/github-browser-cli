bae27c1695d4e419e983f3e9dad2f4659cfcfe98
OrestesK <oresteskassian@gmail.com>
2022-11-20 10:12:12 -0500

    Info line wrapping

--- src/fzf.go
+++ src/fzf.go
@@ -2,7 +2,10 @@ package main
 
 import (
 	"fmt"
+	//"go/format"
 	"log"
+	"strings"
+
 	fuzzyfinder "github.com/ktr0731/go-fuzzyfinder"
 )
 
@@ -10,25 +13,25 @@ import (
 func Fuzzy(repos []Repo) int {
 	idx, err := fuzzyfinder.FindMulti(
 		repos,
-		func(i int) string { 
-            return repos[i].Name 
-        },
+		func(i int) string {
+			return repos[i].Name
+		},
 		fuzzyfinder.WithPreviewWindow(
-            func (i, w, h int) string {
-                if i == -1 {
-                    return ""
-                }
-                return fmt.Sprintf(
-                    "Name: %s\nDesc: %s\nInfo: %s\n",
-                    repos[i].Name,
-                    repos[i].Description,
-                    repos[i].Bar,
-                )
-     }))
+			func(i, w, h int) string {
+				if i == -1 {
+					return ""
+				}
+				return fmt.Sprintf(
+					"Name: %s\nDesc: %s\nInfo: %s\n",
+					repos[i].Name,
+					repos[i].Description,
+					strings.ReplaceAll(repos[i].Bar, "    ", "\n"),
+				)
+			}))
 
 	if err != nil {
-        log.Fatalf("Error performing fuzzy find: %s", err.Error())
+		log.Fatalf("Error performing fuzzy find: %s", err.Error())
 	}
 
-    return idx[0]
+	return idx[0]
 }
